94f31b1bb10741182784a385076798ff12f82379
debalina589 <debalinajana589@gmail.com>
2026-01-09 23:08:45 +0530

    fit: key change project

---
+++ project_7/project7.js
@@ -0,0 +1,21 @@
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class= "color">
+    <table style= "height: 100px; width: 200px">
+  <tr>
+    <th> Key </th>
+    <th> Keycode </th>
+    <th> Code 
+    </th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>     
+    `;
+} );
